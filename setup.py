c55fde14601e6c14caa5c609e26dae042c9969f0
David Cabrero <david.cabrero@madsgroup.org>
2020-09-27 22:34:16 +0200

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='p1-server',
-    version='1.0.0',
-    scripts=['p1server/p1-server.py'],
-    packages=find_packages(),
-    data_files = [('data', ['p1server/intervalos.csv'])],
-    zip_safe=False,
-    install_requires=[
+    name= 'p1-server',
+    version= '1.0.0',
+    scripts= ['p1server/p1-server.py'],
+    packages= find_packages(),
+    package_data= {'p1-server': ['data/intervalos.csv']},
+    zip_safe= False,
+    install_requires= [
         'flask',
     ],
 )
